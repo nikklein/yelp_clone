c5b4ab4a929564f0f6184a428a2d934da7f643e3
nikklein <objcoding@gmail.com>
2016-08-26 16:51:05 +0100

    Update region

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
-  
+
   config.paperclip_defaults = {
   :storage => :s3,
+  :region => ['AWS_REGION='],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
